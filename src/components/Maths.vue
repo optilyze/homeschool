0065b9448c73c54c905c1ee71fdd525684490866
optilyze <tony@optilyze.com.au>
2020-03-25 13:14:57 +1100

    Set type and level

--- src/components/Maths.vue
+++ src/components/Maths.vue
@@ -1,11 +1,99 @@
 <template>
-  <div>MATHS, WOO!</div>
+  <div>
+    <div style="height: 20vh"></div>
+    <div class="row" v-if="quizType&&level">
+      <div class="col col-md-6 offset-md-3">
+        <h4 class="text-center">
+          {{quizType.name}}
+        </h4>
+      </div>
+    </div>
+    <div class="row" v-if="!quizType">
+      <div class="col col-md-6 offset-md-3">
+        <ul class="list-group">
+          <li class="list-group-item">
+            <h4 class="text-center">
+              Select a Quiz Type
+            </h4>
+          </li>
+          <li
+            v-for="qt in quizTypes"
+            :key="qt.code" 
+            class="list-group-item h-lightgrey"
+            @click="setQuizType(qt)"
+          >
+            {{qt.name}}
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="row" v-if="quizType&&!level">
+      <div class="col col-md-6 offset-md-3">
+        <ul class="list-group">
+          <li class="list-group-item">
+            <h4 class="text-center">
+              {{ quizType.name }} Level
+            </h4>
+          </li>
+          <li
+            v-for="l in levels"
+            :key="l" 
+            class="list-group-item h-lightgrey"
+            @click="setLevel(l)"
+          >
+            Level {{l}}
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'Maths',
   props: {
+  },
+  data: function() {
+    return {
+      quizType: false,
+      quizTypes: [
+        {
+          name: "Addition",
+          code: "+"
+        },
+        {
+          name: "Subtraction",
+          code: "-"
+        },
+        {
+          name: "Multiplication",
+          code: "*"
+        },
+        {
+          name: "Division",
+          code: "/"
+        },
+        {
+          name: "Mixed",
+          code: "+-*/"
+        }
+      ],
+      level: false,
+    }
+  },
+  computed: {
+    levels: function() {
+      return _.times(10, n => n+1)
+    }
+  },
+  methods: {
+    setQuizType: function(to) {
+      this.quizType = to;
+    },
+    setLevel: function(to) {
+      this.level = to;
+    }
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './routes'
+import _ from 'lodash';
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './css/utility.css'
